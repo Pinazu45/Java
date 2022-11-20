b04dba97e1f00d2470a1245c8228d982c6ac3674
Pinazu45 <115799704+Pinazu45@users.noreply.github.com>
2022-11-19 23:50:38 -0600

    Con estas ultimas dos adiciones, se muestran las operaciones basicas en java

--- operaciones.java
+++ operaciones.java
@@ -2,10 +2,16 @@ public class operaciones
 {
     public static void main(String[]args)
     {
-        int a = 11, b = 5, s, r;
+        int a = 11, b = 5, s, r, m, d;
         s = a + b; //Se crean variables extras para guardar resultados
         r = a - b;
+        m = a * b;
+        d = a / b;
         System.out.print("El resultado de 11 + 5 es: " + s);
         System.out.print("\nEl resultado de 11 - 5 es: " + r);
+        System.out.print("\nEl resultado de 11 * 5 es: " + m);
+        System.out.print("\nEl resultado de 11 / 5 es: " + d);
+
+        System.out.print("Esta serian las operaciones basicas");
     }
 }
