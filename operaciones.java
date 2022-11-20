6a7b225b473577949f6cd9e8c3419120636b0fe4
Pinazu45 <115799704+Pinazu45@users.noreply.github.com>
2022-11-19 23:43:27 -0600

    las variables varian mucho, asi que cuidado con como las asignas

--- operaciones.java
+++ operaciones.java
@@ -3,7 +3,7 @@ public class operaciones
     public static void main(String[]args)
     {
         int a = 11, b = 5, s, r;
-        s = a + b;
+        s = a + b; //Se crean variables extras para guardar resultados
         r = a - b;
         System.out.print("El resultado de 11 + 5 es: " + s);
         System.out.print("\nEl resultado de 11 - 5 es: " + r);
