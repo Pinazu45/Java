2e609cb5c812bc722c48cea382c489cab8d75a5b
Pinazu45 <115799704+Pinazu45@users.noreply.github.com>
2022-11-19 23:42:16 -0600

    De esta manera se puede ver como sumar y restar

---
+++ operaciones.java
@@ -0,0 +1,11 @@
+public class operaciones
+{
+    public static void main(String[]args)
+    {
+        int a = 11, b = 5, s, r;
+        s = a + b;
+        r = a - b;
+        System.out.print("El resultado de 11 + 5 es: " + s);
+        System.out.print("\nEl resultado de 11 - 5 es: " + r);
+    }
+}
